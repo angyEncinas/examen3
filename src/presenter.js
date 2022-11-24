7d2c749d826a11512109a0b2480d87709f7cccc3
Angy Encinas <angy.encinas@ucb.edu.bo>
2022-11-24 09:35:11 -0400

    muestra la publicacion

--- src/presenter.js
+++ src/presenter.js
@@ -3,12 +3,14 @@ import Publicacion from "./sumador";
 const postdado = document.querySelector("#post");
 const form = document.querySelector("#post-form");
 const div = document.querySelector("#resultado-div");
-pub = new Publicacion();
-let publicaciones = []
+
+let publicaciones = [];
+var mostrar = "hola";
+var contador = 0;
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
   const post = postdado.value;
-  pub.putTexto(post);
-  div.innerHTML = "<p>" +pub.getTexto() + "</p>";
+  publicaciones.push(post);
+  div.innerHTML = "<p>" + publicaciones[contador] + "</p>";
+  contador++;
 });
