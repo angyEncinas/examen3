9dca6fc731d7710a0cf5fd84e018a38afec2bb5b
Angy Encinas <angy.encinas@ucb.edu.bo>
2022-11-24 09:48:16 -0400

    ver publicacion

--- cypress/e2e/sumador.cy.js
+++ cypress/e2e/sumador.cy.js
@@ -3,6 +3,12 @@ describe("Pagina Principal", () => {
     cy.visit("/");
     cy.get("#post").type("Hola mundo");
     cy.get("#publicar-button").click();
-    //cy.get("#resultado-div").should("contain", "Hola mundo");
+    cy.get("#resultado-div").should("contain", "Hola mundo");
+  });
+  it("Deberia mostrar error", () => {
+    cy.visit("/");
+    cy.get("#post").type(" ");
+    cy.get("#publicar-button").click();
+    cy.get("#resultado-div").should("contain", "No se puede ingresar un post sin texto");
   });
 });
--- src/presenter.js
+++ src/presenter.js
@@ -3,14 +3,19 @@ import Publicacion from "./sumador";
 const postdado = document.querySelector("#post");
 const form = document.querySelector("#post-form");
 const div = document.querySelector("#resultado-div");
-
+const div2 = document.querySelector("#alerta-div");
 let publicaciones = [];
 var mostrar = "hola";
 var contador = 0;
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const post = postdado.value;
+  let post = postdado.value;
+  if(post = " "){
+    div2.innerHTML = "<p>" + "No se puede ingresar un post sin texto" + "</p>";
+  }
   publicaciones.push(post);
-  div.innerHTML = "<p>" + publicaciones[contador] + "</p>";
+  div.innerHTML = "<p>" + post + "</p>";
   contador++;
+
+  
 });
